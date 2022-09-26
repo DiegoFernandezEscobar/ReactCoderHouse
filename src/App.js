b60e3c3ba329b9e50432d5f78624a42efd1a8752
Diego FernandezEscobar <fernandezescobardiego@gmail.com>
2022-09-26 19:10:15 -0300

    Primera entrega DiegoFernandezEscobar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header/Header';
 import Body from './components/header/Body';
 
-
+// Mi nombre es Diego Fernandez Escobar, soy el alumno que escribio el codigo, lo aclaro porque me olvide de poner mi nombre en la entrega! Muchas gracias
+// Para realizar el proyecto utilice Boostrap-React
 
 function App() {
   return (
